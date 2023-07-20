9d451fb30f30fa52b548c88aa22061f183326233
Michael Murphy <michael.murphy@enable.com>
2023-07-20 15:05:25 +0100

    Adds prints to for output

--- src/calcs_project/tasks/calculate_average_temperature.py
+++ src/calcs_project/tasks/calculate_average_temperature.py
@@ -1,13 +1,13 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import avg
 
-from pyspark.sql.types import StringType, DateType, IntegerType, StructField, StructType
+DB_NAME = "claims.temp.airport_temperatures_mu"
 
 spark = SparkSession.builder.appName("calcs_project.calculate_average_temperature").getOrCreate()
 
-RESULT_TABLE_NAME = "claims.temp.airport_temperatures_mu"
+print(f"Obtained data from the {DB_NAME} database.")
 
-df = spark.read.table(RESULT_TABLE_NAME)
+df = spark.read.table(DB_NAME)
 averages_df = df.select(avg("TempHighF"), avg("TempLowF")).collect()
 
 print(averages_df)
