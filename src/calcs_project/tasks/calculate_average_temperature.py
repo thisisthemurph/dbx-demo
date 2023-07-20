db9fdd6c0a1ad411fd49128bf0bd52f07f87c6df
Michael Murphy <michael.murphy@enable.com>
2023-07-20 15:40:31 +0100

    Updates average calculation and prints nice message

--- src/calcs_project/tasks/calculate_average_temperature.py
+++ src/calcs_project/tasks/calculate_average_temperature.py
@@ -8,6 +8,14 @@ spark = SparkSession.builder.appName("calcs_project.calculate_average_temperatur
 print(f"Obtained data from the {DB_NAME} database.")
 
 df = spark.read.table(DB_NAME)
-averages_df = df.select(avg("TempHighF"), avg("TempLowF")).collect()
+num_rows = df.count()
 
-print(averages_df)
+averages, = df.select(avg("TempHighF"), avg("TempLowF"))\
+    .withColumnRenamed("avg(TempHighF)", "TempHighF")\
+    .withColumnRenamed("avg(TempLowF)", "TempLowF")\
+    .collect()
+
+avg_temp_low = averages["TempLowF"]
+avg_temp_high = averages["TempHighF"]
+
+print(f"The average airport temperature has a low of {avg_temp_low:.2f}°F and a high of {avg_temp_high:.2f}°F")
